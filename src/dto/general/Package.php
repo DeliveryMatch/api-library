31be94afa76daccf5a1d41e898c1851098953e6f
Rebecca <19052138@student.hhs.nl>
2022-04-25 11:56:15 +0200

    Cleaning

--- src/dto/general/Package.php
+++ src/dto/general/Package.php
@@ -4,14 +4,31 @@ namespace DeliveryMatchApiLibrary\dto\general;
 
 class Package
 {
-    /** @var Package[] */
-    private array $packages;
+    private ?string $warehouse;
+    private string $description;
+    private ?string $type;
+    private float $weight;
+    private float $length;
+    private float $width;
+    private float $height;
 
     /**
-     * @param Package[] $packages
+     * @param string|null $warehouse
+     * @param string $description
+     * @param string|null $type
+     * @param float $weight
+     * @param float $length
+     * @param float $width
+     * @param float $height
      */
-    public function __construct(array $packages)
+    public function __construct(?string $warehouse, string $description, ?string $type, float $weight, float $length, float $width, float $height)
     {
-        $this->packages = $packages;
+        $this->warehouse = $warehouse;
+        $this->description = $description;
+        $this->type = $type;
+        $this->weight = $weight;
+        $this->length = $length;
+        $this->width = $width;
+        $this->height = $height;
     }
 }
